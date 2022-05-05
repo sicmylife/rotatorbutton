c94b77fdff419375cd2297a8f6871abe585c2745
Daniel Daso <sicmylife@gmail.com>
2022-05-05 15:53:52 +0200

    fist commit

---
+++ index.js
@@ -0,0 +1,29 @@
+window.onload=inicio;
+let rotacion=0;
+let sumar=true;
+
+function inicio(){
+    document.querySelector(".boton1").onclick=girar;
+    actualizar();
+}
+
+function girar(){
+    if (sumar){
+        rotacion+=20;  
+    }else {
+        rotacion-=20;
+    } 
+
+    if(rotacion<=0|| rotacion>=360){
+        sumar=!sumar;
+    }
+
+
+    document.querySelector(".flecha").style.transform=`rotate(${rotacion}deg)`;
+actualizar();
+}
+
+
+function actualizar(){
+    document.querySelector(".porcentaje").innerHTML=`${rotacion}*`;
+}
